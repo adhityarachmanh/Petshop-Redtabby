c2fa050e7975b80e6e7d5677c04b82fbdffe282a
Adhitya Rachman <adhityarachman@gmail.com>
2019-08-15 14:00:28 +0700

    First Commit

--- admin/src/config/fbConfig.js
+++ admin/src/config/fbConfig.js
@@ -4,13 +4,7 @@ import "firebase/auth";
 import "firebase/storage";
 
 var fbConfig = {
-  apiKey: "AIzaSyCXCDHLJ5P0Nyx6OkAGufBBFhqQxs46KkM",
-  authDomain: "xena-58c4b.firebaseapp.com",
-  databaseURL: "https://xena-58c4b.firebaseio.com",
-  projectId: "xena-58c4b",
-  storageBucket: "xena-58c4b.appspot.com",
-  messagingSenderId: "405333580507",
-  appId: "1:405333580507:web:ad7aa967f22ca679"
+
 };
 
 firebase.initializeApp(fbConfig);
