429a120ca6f8047738deb0a77b1166702ec28625
adhityarachmanh <53608645+adhityarachmanh@users.noreply.github.com>
2020-08-06 10:47:49 +0700

    Update fbConfig.js

--- client/src/config/fbConfig.js
+++ client/src/config/fbConfig.js
@@ -3,13 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCXCDHLJ5P0Nyx6OkAGufBBFhqQxs46KkM",
-  authDomain: "xena-58c4b.firebaseapp.com",
-  databaseURL: "https://xena-58c4b.firebaseio.com",
-  projectId: "xena-58c4b",
-  storageBucket: "xena-58c4b.appspot.com",
-  messagingSenderId: "405333580507",
-  appId: "1:405333580507:web:ad7aa967f22ca679"
+
 };
 
 firebase.initializeApp(firebaseConfig);
